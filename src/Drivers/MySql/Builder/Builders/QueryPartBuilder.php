4497e7d6de8f7ec1193b06a1ea806e7975c67457
Aleksandar Panic <arekusanda1@gmail.com>
2021-08-22 23:07:16 +0200

    Refactor part builder

--- src/Drivers/MySql/Builder/Builders/QueryPartBuilder.php
+++ src/Drivers/MySql/Builder/Builders/QueryPartBuilder.php
@@ -30,66 +30,6 @@ use ArekX\PQL\RawQueryResult;
 abstract class QueryPartBuilder implements QueryBuilder
 {
     /**
-     * @inheritDoc
-     */
-    public function build(StructuredQuery $query, QueryBuilderState $state = null): RawQuery
-    {
-        /** @var MySqlQueryBuilderState $state */
-
-        if ($state === null) {
-            throw new \Exception('Passed state cannot be null.');
-        }
-
-        $input = $query->toArray();
-        $results = $this->getInitialParts();
-
-        $requiredParts = $this->getRequiredParts();
-
-        foreach ($this->getPartBuilders() as $partName => $buildPart) {
-            if (empty($input[$partName])) {
-
-                if (in_array($partName, $requiredParts)) {
-                    throw new \Exception("Part '${partName}' is required.");
-                }
-
-                continue;
-            }
-
-            $result = $this->buildPart($input[$partName], $buildPart, $state);
-
-            if ($result !== null) {
-                $results[] = $result;
-            }
-        }
-
-        return RawQueryResult::create(
-            implode($state->getQueryPartGlue(), $results),
-            $state->getParamsBuilder()->build(),
-            $query->get('config') ?? null
-        );
-    }
-
-    /**
-     * Return initial parts for the query
-     *
-     * @return array
-     */
-    protected abstract function getInitialParts(): array;
-
-    /**
-     * Return part builders for resolving the query parts to string.
-     *
-     * @return array
-     */
-    protected abstract function getPartBuilders(): array;
-
-    /**
-     * Return parts which must be set in the query in order to be built.
-     * @return array
-     */
-    protected abstract function getRequiredParts(): array;
-
-    /**
      * Build a specific query part.
      *
      * If the part is a structured query it will be built using a parent query builder.
@@ -107,4 +47,78 @@ abstract class QueryPartBuilder implements QueryBuilder
 
         return $buildPart($part, $state);
     }
+
+    /**
+     * @inheritDoc
+     */
+    public function build(StructuredQuery $query, QueryBuilderState $state = null): RawQuery
+    {
+        /** @var MySqlQueryBuilderState $state */
+
+        if ($state === null) {
+            throw new \Exception('Passed state cannot be null.');
+        }
+
+        $input = $query->toArray();
+        $results = implode($state->getQueryPartGlue(), $this->buildQueryParts($input, $state));
+
+        return RawQueryResult::create(
+            $results,
+            $state->getParamsBuilder()->build(),
+            $query->get('config') ?? null
+        );
+    }
+
+    /**
+     * Build each part from the structured query
+     *
+     * @param array $parts Parts from the structured query
+     * @param MySqlQueryBuilderState $state Builder state
+     * @return array Resulting array for each built part.
+     * @throws \Exception
+     */
+    protected function buildQueryParts(array $parts, MySqlQueryBuilderState $state): array
+    {
+        $results = $this->getInitialParts();
+        $requiredParts = $this->getRequiredParts();
+
+        foreach ($this->getPartBuilders() as $partName => $buildPart) {
+            if (empty($parts[$partName])) {
+
+                if (in_array($partName, $requiredParts)) {
+                    throw new \Exception("Part '${partName}' is required.");
+                }
+
+                continue;
+            }
+
+            $result = $this->buildPart($parts[$partName], $buildPart, $state);
+
+            if ($result !== null) {
+                $results[] = $result;
+            }
+        }
+
+        return $results;
+    }
+
+    /**
+     * Return initial parts for the query
+     *
+     * @return array
+     */
+    protected abstract function getInitialParts(): array;
+
+    /**
+     * Return parts which must be set in the query in order to be built.
+     * @return array
+     */
+    protected abstract function getRequiredParts(): array;
+
+    /**
+     * Return part builders for resolving the query parts to string.
+     *
+     * @return array
+     */
+    protected abstract function getPartBuilders(): array;
 }
